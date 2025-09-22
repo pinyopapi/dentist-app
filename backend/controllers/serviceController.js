c1d2ed401f5dfce4d8522a65172205fc90dd0abe
pinyopapi <95istvan@gmail.com>
2025-09-22 18:37:08 +0200

    Add serviceConteroller

---
+++ backend/controllers/serviceController.js
@@ -0,0 +1,7 @@
+export const getServices = async (req, res) => {
+  res.json({ message: "getServices works" });
+};
+
+export const createService = async (req, res) => {
+  res.json({ message: "createService works" });
+};
