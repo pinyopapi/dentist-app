39ab7c292c648a839fd571889757a564e3bbf676
pinyopapi <95istvan@gmail.com>
2025-09-22 18:49:13 +0200

    Create appointmentController base

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,7 @@
+export const getAppointments = async (req, res) => {
+  res.json({ message: "getAppointments works" });
+};
+
+export const createAppointment = async (req, res) => {
+  res.json({ message: "createAppointment works" });
+};
