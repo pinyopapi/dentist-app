a73f2566ec9e739d44ce7a25c0d7d8bb4480a131
pinyopapi <95istvan@gmail.com>
2025-09-22 19:54:58 +0200

    Expand Service controller

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,7 +1,25 @@
+import { getAllServices, addService, updateService, deleteService } from "../models/Service.js";
+
 export const getServices = async (req, res) => {
-  res.json({ message: "getServices works" });
+  const services = await getAllServices();
+  res.json(services);
 };
 
 export const createService = async (req, res) => {
-  res.json({ message: "createService works" });
+  const { name, price } = req.body;
+  const service = await addService(name, price);
+  res.status(201).json(service);
+};
+
+export const editService = async (req, res) => {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const service = await updateService(id, name, price);
+  res.json(service);
+};
+
+export const removeService = async (req, res) => {
+  const { id } = req.params;
+  await deleteService(id);
+  res.json({ message: "Service deleted" });
 };
