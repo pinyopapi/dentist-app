17b1f1812b6acf4ddff1ae05286b5eba39e8e68f
pinyopapi <95istvan@gmail.com>
2025-09-22 17:01:18 +0200

    Change index.js base

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const { AppDataSource } = require("./db");
-
-const authRoutes = require("./routes/authRoutes");
-const servicesRoutes = require("./routes/servicesRoutes");
-const appointmentsRoutes = require("./routes/appointmentsRoutes");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
@@ -13,13 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
-app.use("/services", servicesRoutes);
-app.use("/appointments", appointmentsRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-AppDataSource.initialize()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.log(err));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
