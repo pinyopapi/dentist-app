11dc65a53bb9caf0105e8fc91957eecea1cc747a
pinyopapi <95istvan@gmail.com>
2025-09-22 17:50:53 +0200

    Extend routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/authRoutes.js";
+import servicesRoutes from "./routes/servicesRoutes.js";
+import appointmentsRoutes from "./routes/appointmentsRoutes.js";
+
+import { AppDataSource } from "./db.js";
 
 dotenv.config();
 const app = express();
@@ -9,6 +14,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/services", servicesRoutes);
+app.use("/appointments", appointmentsRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
