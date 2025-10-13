1e0ecba8e68edc4fe3431efe0f30b711b700f152
pinyopapi <95istvan@gmail.com>
2025-10-13 11:57:54 +0200

    Create authService

---
+++ frontend/src/services/authService.js
@@ -0,0 +1,19 @@
+const API_URL = "/auth";    // modified proxy in package.json, so shortenning works
+
+export async function register(userData) {
+  const res = await fetch(`${API_URL}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(userData),
+  });
+  return await res.json();
+}
+
+export async function login(credentials) {
+  const res = await fetch(`${API_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(credentials),
+  });
+  return await res.json();
+}
