c199b0a646b7c2347313cff09f5dc94dfd584d40
pinyopapi <95istvan@gmail.com>
2025-10-13 17:28:03 +0200

    Add Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,31 @@
-import { LanguageProvider } from "./contexts/LanguageContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 import LanguageSelector from "./components/LanguageSelector";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <AuthProvider>
       <LanguageProvider>
-        <LanguageSelector />
-        <RegisterPage />
-        <LoginPage />
+        <Router>
+          <LanguageSelector />
+          <Routes>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route
+              path="/dashboard"
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Router>
       </LanguageProvider>
     </AuthProvider>
   );
