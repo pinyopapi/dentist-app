e542308faea4e21ec0fbda2174f5e6f02b079368
pinyopapi <95istvan@gmail.com>
2025-10-13 16:34:35 +0200

    Wrap pages with auth provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import { LanguageProvider } from "./contexts/LanguageContext";
+import { AuthProvider } from "./contexts/AuthContext";
 import LanguageSelector from "./components/LanguageSelector";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <LanguageProvider>
+    <AuthProvider>
+      <LanguageProvider>
         <LanguageSelector />
         <RegisterPage />
         <LoginPage />
-    </LanguageProvider>
+      </LanguageProvider>
+    </AuthProvider>
   );
 }
 
