b5deae868871c864bb8cc32637c52b14510860e3
pinyopapi <95istvan@gmail.com>
2025-10-13 12:40:10 +0200

    Define main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
+import { useContext } from "react";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import { LanguageProvider, LanguageContext } from "./contexts/LanguageContext";
+
+function LanguageSwitcher() {
+  const { language, toggleLanguage } = useContext(LanguageContext);
+  return (
+    <button onClick={toggleLanguage}>
+      {language === "hu" ? "English" : "Magyar"}
+    </button>
+  );
+}
+
+function AppContent() {
+  return (
+    <div>
+      <LanguageSwitcher />
+      <RegisterPage />
+      <LoginPage />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <h1>Dentist App</h1>
-    </div>
+    <LanguageProvider>
+      <AppContent />
+    </LanguageProvider>
   );
 }
 
