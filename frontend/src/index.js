f7d1b747603bb4ab3cb4bb81d62b16981558b2dc
pinyopapi <95istvan@gmail.com>
2025-10-13 14:14:42 +0200

    Wrap App with googleprovider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
+
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <GoogleOAuthProvider clientId={clientId}>
     <App />
+  </GoogleOAuthProvider>
 );
