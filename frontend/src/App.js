8e9c4a69e82edd7bde2a298dba0d42b8bc5442fd
pinyopapi <95istvan@gmail.com>
2025-10-13 20:21:05 +0200

    Add navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
-import LanguageSelector from "./components/LanguageSelector";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   return (
     <AuthProvider>
       <LanguageProvider>
         <Router>
-          <LanguageSelector />
+          <Navbar />
           <Routes>
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/dashboard"
+            <Route path="/dashboard"
               element={
                 <ProtectedRoute>
                   <Dashboard />
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/AuthContext";
+import LanguageSelector from ".//LanguageSelector";
+
+const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        logout();
+        navigate("/login");
+    };
+
+    return (
+        <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+            <Link to="/" style={{ marginRight: "15px" }}>Home</Link>
+
+            {user ? (
+                <>
+                    <span style={{ marginRight: "15px" }}>Hello, {user.name}</span>
+                    <button onClick={handleLogout}>Logout</button>
+                </>
+            ) : (
+                <>
+                    <Link to="/login" style={{ marginRight: "10px" }}>Login</Link>
+                    <Link to="/register">Register</Link>
+                </>
+            )}
+          <LanguageSelector />
+        </nav>
+    );
+};
+
+export default Navbar;
