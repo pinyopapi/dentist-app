e1a6d180151543fdac1fa1df03a4725fb853a000
pinyopapi <95istvan@gmail.com>
2025-10-13 12:47:17 +0200

    Refactor LanguageContext

--- frontend/src/contexts/LanguageContext.js
+++ frontend/src/contexts/LanguageContext.js
@@ -2,16 +2,12 @@ import { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export function LanguageProvider({ children }) {
+export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState("hu");
 
-  const toggleLanguage = () => {
-    setLanguage((prev) => (prev === "hu" ? "en" : "hu"));
-  };
-
   return (
-    <LanguageContext.Provider value={{ language, toggleLanguage }}>
+    <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
-}
+};
