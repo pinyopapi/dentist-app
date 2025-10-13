e012173a33678f76973c3d2f26ef4dd8ca232ce9
pinyopapi <95istvan@gmail.com>
2025-10-13 12:47:30 +0200

    Refactor App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,14 @@
-import { useContext } from "react";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import LanguageSelector from "./components/LanguageSelector";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
-import { LanguageProvider, LanguageContext } from "./contexts/LanguageContext";
-
-function LanguageSwitcher() {
-  const { language, toggleLanguage } = useContext(LanguageContext);
-  return (
-    <button onClick={toggleLanguage}>
-      {language === "hu" ? "English" : "Magyar"}
-    </button>
-  );
-}
-
-function AppContent() {
-  return (
-    <div>
-      <LanguageSwitcher />
-      <RegisterPage />
-      <LoginPage />
-    </div>
-  );
-}
 
 function App() {
   return (
     <LanguageProvider>
-      <AppContent />
+        <LanguageSelector />
+        <RegisterPage />
+        <LoginPage />
     </LanguageProvider>
   );
 }
