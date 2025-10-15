a3517647e49cc7bde544e22949be88ce0608c483
pinyopapi <95istvan@gmail.com>
2025-10-15 19:53:35 +0200

    Add deleted imports

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { Navigate } from "react-router-dom";
+
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
-  if (loading) return <p>Loading...</p>; 
+  if (loading) return <p>Loading...</p>;
 
   return user ? children : <Navigate to="/login" />;
 };
+
+export default ProtectedRoute;
