7e36bc112c78218783f0eb03d2ff34839c110ee1
pinyopapi <95istvan@gmail.com>
2025-10-15 12:36:33 +0200

    Add calendar delete route

--- backend/routes/calendarRoutes.js
+++ backend/routes/calendarRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getGoogleCalendarEvents, createGoogleCalendarEvent, bookGoogleCalendarSlot } from "../controllers/calendarController.js";
+import { getGoogleCalendarEvents, createGoogleCalendarEvent, bookGoogleCalendarSlot, deleteGoogleCalendarEvent } from "../controllers/calendarController.js";
 
 const router = express.Router();
 
 router.get("/events", getGoogleCalendarEvents);
 router.post("/create", createGoogleCalendarEvent);
 router.post("/book", bookGoogleCalendarSlot);
+router.post("delete", deleteGoogleCalendarEvent);
 
 export default router;
