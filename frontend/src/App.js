d234febef1219d487a88ba7b185159d4c52c5992
pinyopapi <95istvan@gmail.com>
2025-10-15 14:15:47 +0200

    Add google provider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./contexts/AuthContext";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
 import AppointmentsPage from "./pages/AppointmentsPage";
 import AdminPage from "./pages/AdminPage";
-
+import ProtectedRoute from "./components/ProtectedRoute";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <AuthProvider>
-      <LanguageProvider>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
-            <Route path="/appointments" element={<ProtectedRoute> <AppointmentsPage /> </ProtectedRoute>} />
-            <Route path="/admin" element={<ProtectedRoute> <AdminPage /> </ProtectedRoute>} />
-          </Routes>
-        </Router>
-      </LanguageProvider>
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <LanguageProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/register" element={<RegisterPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /> </ProtectedRoute>} />
+              <Route path="/appointments" element={<ProtectedRoute> <AppointmentsPage /> </ProtectedRoute>} />
+              <Route path="/admin" element={<ProtectedRoute> <AdminPage /> </ProtectedRoute>} />
+            </Routes>
+          </Router>
+        </LanguageProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
