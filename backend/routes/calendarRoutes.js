2837d041ef4f0187717f138b240ab0fe918f423b
pinyopapi <95istvan@gmail.com>
2025-10-15 11:14:13 +0200

    Remove free-slot route

--- backend/routes/calendarRoutes.js
+++ backend/routes/calendarRoutes.js
@@ -1,11 +1,10 @@
 import express from "express";
-import { getGoogleCalendarEvents, createGoogleCalendarEvent, createFreeSlot } from "../controllers/calendarController.js";
+import { getGoogleCalendarEvents, createGoogleCalendarEvent, bookGoogleCalendarSlot } from "../controllers/calendarController.js";
 
 const router = express.Router();
 
 router.get("/events", getGoogleCalendarEvents);
 router.post("/create", createGoogleCalendarEvent);
-router.post("/free-slot", createFreeSlot);
 router.post("/book", bookGoogleCalendarSlot);
 
 export default router;
