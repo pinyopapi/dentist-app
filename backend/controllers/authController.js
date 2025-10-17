278da1a46c0061a864eca89f5ac9b306d18fa656
pinyopapi <95istvan@gmail.com>
2025-10-17 10:22:20 +0200

    Expand wiht admin check

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,23 +3,25 @@ import jwt from "jsonwebtoken";
 import { getUserByEmail, addUser } from "../models/User.js";
 import { OAuth2Client } from "google-auth-library";
 
-// Email/pwd register
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
+
     const existingUser = await getUserByEmail(email);
     if (existingUser) return res.status(400).json({ message: "User exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    await addUser(name, email, hashedPassword);
-    res.status(201).json({ message: "User registered" });
+    const newUser = await addUser(name, email, hashedPassword);
+
+    res.status(201).json({ message: "User registered", user: { name: newUser.name, email: newUser.email, isAdmin: newUser.is_admin } });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-// Email/pwd login
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -30,20 +32,17 @@ export const login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
 
     const jwtToken = jwt.sign(
-      { email: user.email },
+      { email: user.email, isAdmin: user.is_admin },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    res.json({ jwtToken, user: { name: user.name, email: user.email } });
+    res.json({ jwtToken, user: { name: user.name, email: user.email, isAdmin: user.is_admin } });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-// Google OAuth
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 export const googleLogin = async (req, res) => {
   try {
@@ -64,12 +63,12 @@ export const googleLogin = async (req, res) => {
     }
 
     const jwtToken = jwt.sign(
-      { email: user.email, name: user.name },
+      { email: user.email, isAdmin: user.is_admin },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
 
-    res.json({ jwtToken, user: { name: name, email: email } });
+    res.json({ jwtToken, user: { name: user.name, email: user.email, isAdmin: user.is_admin } });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Google authentication failed" });
